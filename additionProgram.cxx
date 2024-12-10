ce3c71609e8bc5a3eb85a6e2f2e7a3271593202c
Godbless Ikadoi <godblessika@gmail.com>
2024-12-11 00:39:32 +0100

    add program instructions

---
+++ additionProgram.cxx
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int number1, number2, sum;
+
+    cout << "Enter number: " << endl;
+    cin >> number1 >> number2;
+
+    sum = number1 + number2;
+
+    cout << "sum is: " << sum << endl;
+}
