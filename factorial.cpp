8401c0fb6b5edd5afa0caa75e17e506a672067cc
Godbless Ikadoi <godblessika@gmail.com>
2025-01-05 00:07:42 +0100

    add new projects

--- factorial.cpp
+++ factorial.cpp
@@ -1,20 +1,20 @@
-#include <iostream> 
+#include <iostream>
 using namespace std;
 
 int main()
 {
 	int n;
-	
-	cout << "Enter number: " <<endl;
+
+	// cout << "Enter number: " <<endl;
 	cin >> n;
-	
-	if(n == 0)
+
+	if (n == 0)
 	{
 		return 1;
 	}
 	else
 	{
-		cout << n * (n - 1) <<endl;
+		cout << n * (n - 1) << endl;
 	}
 	return 0;
 }
